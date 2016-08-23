add21def417d1d27180cf679709b7e33fd9eb24e
Eduardo de Godoy Ferrari <eduardo___ferrari@outlook.com>
2016-08-23 14:17:30 -0300

    Entity NotaFiscal e ItemNotaFiscal

--- src/main/java/br/com/system/gestaoConstrucaoCivil/entity/ItemNotaFiscal.java
+++ src/main/java/br/com/system/gestaoConstrucaoCivil/entity/ItemNotaFiscal.java
@@ -1,6 +1,9 @@
 package br.com.system.gestaoConstrucaoCivil.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
@@ -10,5 +13,54 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @Table(name = "item_nota_fiscal")
 public class ItemNotaFiscal extends AbstractPersistable<Long>{
 
+	
+	private Produto produto;
+	private UnidadeMedida unidadeMedida;
+	private Integer quantidade;
+	private Double valorUnitario;
+	private Double valorTotal;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "nota_fiscal")
+	private NotaFiscal notaFiscal;
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public UnidadeMedida getUnidadeMedida() {
+		return unidadeMedida;
+	}
+	public void setUnidadeMedida(UnidadeMedida unidadeMedida) {
+		this.unidadeMedida = unidadeMedida;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Double getValorUnitario() {
+		return valorUnitario;
+	}
+	public void setValorUnitario(Double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	public NotaFiscal getNotaFiscal() {
+		return notaFiscal;
+	}
+	public void setNotaFiscal(NotaFiscal notaFiscal) {
+		this.notaFiscal = notaFiscal;
+	}
+	
+	
 	
 }
--- src/main/java/br/com/system/gestaoConstrucaoCivil/entity/NotaFiscal.java
+++ src/main/java/br/com/system/gestaoConstrucaoCivil/entity/NotaFiscal.java
@@ -1,13 +1,115 @@
 package br.com.system.gestaoConstrucaoCivil.entity;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.system.gestaoConstrucaoCivil.enuns.TipoNota;
 
 @Entity
 @Table(name = "nota_fiscal")
 public class NotaFiscal extends AbstractPersistable<Long>{
 
+	private TipoNota tipoNota;
+	private Integer numero;
+	private Integer chaveAcesso;
+	private Date dataNota;
+	private Date dataVencimento;
+	private Integer serie;
+	private Fornecedor fornecedor;
+	private String observacao;
+	private Double valorTotal;
+	
+	@OneToMany(mappedBy = "notaFiscal" , cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+	private List<ItemNotaFiscal> itens;
+
+	public TipoNota getTipoNota() {
+		return tipoNota;
+	}
+    
+	public Integer getChaveAcesso() {
+		return chaveAcesso;
+	}
+
+	public void setChaveAcesso(Integer chaveAcesso) {
+		this.chaveAcesso = chaveAcesso;
+	}
+
+	public void setTipoNota(TipoNota tipoNota) {
+		this.tipoNota = tipoNota;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Date getDataNota() {
+		return dataNota;
+	}
+
+	public void setDataNota(Date dataNota) {
+		this.dataNota = dataNota;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	public Integer getSerie() {
+		return serie;
+	}
+
+	public void setSerie(Integer serie) {
+		this.serie = serie;
+	}
+	
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public List<ItemNotaFiscal> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ItemNotaFiscal> itens) {
+		this.itens = itens;
+	}
+
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+	
 	
 }
