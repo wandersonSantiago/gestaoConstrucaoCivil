ea382c862cc1aab99cbc69e9212cacefe6c76bfb
wandersonSantiago <wandersonSantiago@192.168.0.77>
2016-08-23 23:50:00 -0300

    lista de fornecedores nos produtos

--- src/main/resources/static/public/js/pages/controller/produto-controller.js
+++ src/main/resources/static/public/js/pages/controller/produto-controller.js
@@ -1,7 +1,7 @@
 app.controller('produtoController', function($scope,produtoService, $routeParams){
 	
 	var self = this;
-		
+		$scope.listaFornecedores = [];
 	
 	 self.cadastrarProduto = function(produto){
 			 produtoService.produtoCreate(self.produto);
@@ -17,6 +17,35 @@ app.controller('produtoController', function($scope,produtoService, $routeParams
 			});
 		};
 	
-	  
+		//cria uma lista de fornecedores
+		self.adicionarFornecedores = function(){
+			$scope.listaFornecedores.push({
+				razaoSocial : self.fornecedor.dadoEmpresa.razaoSocial ,
+				nomeFantasia : self.fornecedor.dadoEmpresa.nomeFantasia ,
+				cnpj : self.fornecedor.dadoEmpresa.cnpj
+			});
+			console.log($scope.listaFornecedores);
+						fornecedor.value = "";
+						$scope.fornecedor = null;
+		}
+		
+		self.ativarExcluirLote = function(listaFornecedores){
+			$scope.listaFornecedores.filter(function(fornecedor){
+			if(listaFornecedores.selecionado){
+				$scope.ativadoExcluirLote = true;
+			}
+			});
+		}
+			
+
+		//apagar outros empreendimentos, somente da lista de front
+		self.apagarFornecedores = function(fornecedor){
+				$scope.fornecedor = fornecedor.filter(function(outro){
+				if(!outro.fornecedor) return outro;
+				$scope.ativadoExcluirLote = null;
+			});
+			
+			
+		}
 	
 });
