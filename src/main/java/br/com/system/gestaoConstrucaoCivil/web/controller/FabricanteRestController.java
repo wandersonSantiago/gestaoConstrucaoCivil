11e9a72a52664678d87734457b9085dfa15b90a4
Eduardo de Godoy Ferrari <eduardo___ferrari@outlook.com>
2016-09-03 15:52:53 -0300

    Fabricante,FabricanteRepository,FabricanteService e FabricanteRestController

---
+++ src/main/java/br/com/system/gestaoConstrucaoCivil/web/controller/FabricanteRestController.java
@@ -0,0 +1,54 @@
+package br.com.system.gestaoConstrucaoCivil.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.system.gestaoConstrucaoCivil.entity.Fabricante;
+import br.com.system.gestaoConstrucaoCivil.service.FabricanteService;
+
+@RestController
+@RequestMapping("/rest/almoxarifado/fabricante")
+public class FabricanteRestController {
+
+	@Autowired
+	private FabricanteService fabricanteService;
+	
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/lista")
+	public ResponseEntity<Iterable<Fabricante>> buscarFabricantes() {
+		Iterable<Fabricante> fabricantes = fabricanteService.buscarTodos();
+		return new ResponseEntity<Iterable<Fabricante>>(fabricantes, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/buscaPorId/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Fabricante> buscarFabricantePorId(@PathVariable Long id) {
+
+		return new ResponseEntity<Fabricante>(fabricanteService.buscarPorId(id), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/salva", method = RequestMethod.POST)
+	public ResponseEntity salvarFabricante(@RequestBody Fabricante fabricante, UriComponentsBuilder ucBuilder) {
+		fabricanteService.salvarOuEditar(fabricante);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(
+				ucBuilder.path("rest/almoxarifado/fabricante/salva/{id}").buildAndExpand(fabricante.getId()).toUri());
+		return new ResponseEntity(headers, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/altera", method = RequestMethod.PUT)
+	public ResponseEntity alterarFabricante(@RequestBody Fabricante fabricante, UriComponentsBuilder ucBuilder) {
+		fabricanteService.salvarOuEditar(fabricante);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(ucBuilder.path("rest/almoxarifado/fabricante/altera/{fabricante}")
+				.buildAndExpand(fabricante.getId()).toUri());
+		return new ResponseEntity(headers, HttpStatus.CREATED);
+	}
+}
