e14ebfbf274b6caf1db66c77ec6aa9317a3fb46d
Eduardo de Godoy Ferrari <eduardo___ferrari@outlook.com>
2016-09-13 14:15:34 -0300

    Correção no controller DadoEmpresaRestController

--- src/main/java/br/com/system/gestaoConstrucaoCivil/web/controller/DadoEmpresaRestController.java
+++ src/main/java/br/com/system/gestaoConstrucaoCivil/web/controller/DadoEmpresaRestController.java
@@ -3,6 +3,7 @@ package br.com.system.gestaoConstrucaoCivil.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,13 +20,13 @@ public class DadoEmpresaRestController {
 	 private DadoEmpresaService dadoEmpresaService;
 	 
 	 @RequestMapping(method = RequestMethod.GET, value = "/existeCnpj/{cnpj}")
-		public ResponseEntity existeCnpj(@RequestBody String cnpj) {
+		public ResponseEntity existeCnpj(@PathVariable String cnpj) {
 			System.out.println(cnpj);
 			 return new ResponseEntity(dadoEmpresaService.existeCnpjCadastrado(cnpj), HttpStatus.OK);
 
 		}
 		@RequestMapping(method = RequestMethod.GET, value = "/existeIe/{ie}")
-		public ResponseEntity existeIe(@RequestBody String ie) {
+		public ResponseEntity existeIe(@PathVariable String ie) {
 			
 			 return new ResponseEntity(dadoEmpresaService.existeIeCadastrado(ie), HttpStatus.OK);
 
