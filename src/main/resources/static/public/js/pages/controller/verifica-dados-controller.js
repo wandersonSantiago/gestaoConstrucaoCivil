681b06392f752998ebd76db672f77b02b6b1779f
wandersonSantiago <wandersonSantiago>
2016-09-13 15:53:08 -0300

    finalizada a verificação do cnpj e ie da empresa contratante

--- src/main/resources/static/public/js/pages/controller/verifica-dados-controller.js
+++ src/main/resources/static/public/js/pages/controller/verifica-dados-controller.js
@@ -7,18 +7,31 @@ app.controller('verificaDadosController', function($scope, toastr, verificaDados
 			console.log(cnpj);
 			verificaDadosService.verificaCnpj(cnpj).
 			then(function(p){
-				self.verificado = p;
-				console.log(self.verificado);
+				self.verificado = p;				
+				if(self.verificado){
+					sweetAlert({ timer : 105000,  text :"CNPJ ja consta cadastrado em nosso banco de dados",  type : "info", width: 300, higth: 300, padding: 20});
+					self.limpaCampo();
+				}
+				
 				}, function(errResponse){
 			});
 		};
 		self.verificaIe = function(ie){
 			verificaDadosService.verificaIe(ie).
 			then(function(p){
-				self.usuario = p;
+				self.verificado = p;				
+				if(self.verificado){
+					sweetAlert({ timer : 105000,  text :"Inscrição Estadual ja consta cadastrado em nosso banco de dados",  type : "info", width: 300, higth: 300, padding: 20});
+					self.limpaCampo();
+					
+				}
 				}, function(errResponse){
 			});
 		};
 	
+		self.limpaCampo = function(){
+			$scope.adminCtrl.empresa.dadoEmpresa.cnpj = null;
+			$scope.adminCtrl.empresa.dadoEmpresa.inscricaoEstadual = null;
+		}
 	
 });
--- src/main/resources/static/public/js/pages/servicos/veirifica-dados-service.js
+++ src/main/resources/static/public/js/pages/servicos/veirifica-dados-service.js
@@ -18,6 +18,7 @@ app.factory('verificaDadosService', function($rootScope, $http,$q){
 			return $http.get('/rest/dadoEmpresa/existeIe/'+param)
 			.then(function(response){
 				return response.data;
+				console.log(response);
 			},function(errResponse){
 				sweetAlert({ timer : 3000,  text :"falha na conexão",  type : "error", width: 300, higth: 300, padding: 20});
 				return $q.reject(errResponse);
