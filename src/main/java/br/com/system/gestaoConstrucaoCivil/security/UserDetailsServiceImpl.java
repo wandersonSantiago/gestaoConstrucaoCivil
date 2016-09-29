e65e6d5d485b7154ff0485f044c468671558a441
Eduardo de Godoy Ferrari <eduardo___ferrari@outlook.com>
2016-09-29 10:10:10 -0300

    UserDetailsServiceImpl

--- src/main/java/br/com/system/gestaoConstrucaoCivil/security/UserDetailsServiceImpl.java
+++ src/main/java/br/com/system/gestaoConstrucaoCivil/security/UserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package br.com.system.gestaoConstrucaoCivil.security;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,24 +12,21 @@ import br.com.system.gestaoConstrucaoCivil.service.UsuarioService;
 
 @Service
 @Transactional
-public class UserDetailsServiceImpl implements UserDetailsService{
+public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
-	private UsuarioService usuarioService;	
-	
+	private UsuarioService usuarioService;
+
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {	
-		
-		System.out.println("Metodo loadUserByUsername");
-		Usuario usuario = usuario = usuarioService.buscarPorLogin(username);
-		if(usuario != null)
-		{
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		Usuario usuario = usuarioService.buscarPorLogin(username);
+		if (usuario != null) {
 			UserDetailsImpl user = new UserDetailsImpl();
-			//user.setUsername(usuario.getId().toString());
+
 			user.setUsername(usuario.getLogin());
 			user.setPassword(usuario.getSenha());
-		 
-			
+
 			return user;
 		}
 		throw new UsernameNotFoundException("Usuario não encontrado");
