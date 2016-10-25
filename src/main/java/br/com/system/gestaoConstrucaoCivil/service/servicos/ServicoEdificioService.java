67c770f363bf0c53085d5248ca40f9e0fef21f48
Eduardo de Godoy Ferrari <eduardo___ferrari@outlook.com>
2016-10-25 13:39:20 -0200

    Verificando o vingular pacote

--- src/main/java/br/com/system/gestaoConstrucaoCivil/service/servicos/ServicoEdificioService.java
+++ src/main/java/br/com/system/gestaoConstrucaoCivil/service/servicos/ServicoEdificioService.java
@@ -2,34 +2,51 @@ package br.com.system.gestaoConstrucaoCivil.service.servicos;
 
 import java.util.List;
 
+import javax.websocket.Session;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import br.com.system.gestaoConstrucaoCivil.entity.Empreendimento;
 import br.com.system.gestaoConstrucaoCivil.entity.servicos.ServicoEdificio;
+import br.com.system.gestaoConstrucaoCivil.pojo.SessionUsuario;
 import br.com.system.gestaoConstrucaoCivil.repository.servicos.ServicoEdificioRepository;
-import br.com.system.gestaoConstrucaoCivil.service.EmpreendimentoService;
 
 @Service
-@Transactional(readOnly = true,propagation = Propagation.REQUIRED)
-public class ServicoEdificioService  {
+@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
+public class ServicoEdificioService {
 
 	@Autowired
 	private ServicoEdificioRepository servicoEdificioRepository;
-	@Autowired
-	private EmpreendimentoService empreendimentoService;
+
 	public List<ServicoEdificio> buscarTodos() {
- 		
- 		return servicoEdificioRepository.findAll();
+
+		return servicoEdificioRepository.findAll();
 	}
-	
+
+	private boolean verificarExistePacoteParaEmpresa(ServicoEdificio servico) {
+		List<ServicoEdificio> servicosEdificios = servicoEdificioRepository.findAll();
+
+		for (ServicoEdificio s : servicosEdificios) {
+
+			if (servico.getTorre() == s.getTorre() && servico.getApartamento() == s.getApartamento()
+					&& s.getPacoteServico().equals(servico.getPacoteServico()))
+
+			{
+
+				return true;
+
+			}
+
+		}
+		return false;
+	}
+
 	@Transactional(readOnly = false)
-	public void salvarOuEditar(ServicoEdificio servico)
-	{
-		List<Empreendimento> empreendimento = empreendimentoService.buscarTodos();
-		servico.setEmpreendimento(empreendimento.get(0));
-		servicoEdificioRepository.save(servico);
+	public void salvarOuEditar(ServicoEdificio servico) {
+		servico.setEmpreendimento(SessionUsuario.getInstance().getUsuario().getEmpreendimento());
+		if (verificarExistePacoteParaEmpresa(servico) != true) {
+			servicoEdificioRepository.save(servico);
+		}
 	}
 }
