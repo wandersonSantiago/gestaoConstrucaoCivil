522625265d4e258d3c4189c27ae848be466a7524
Eduardo de Godoy Ferrari <eduardo___ferrari@outlook.com>
2016-10-29 13:44:37 -0200

    ItemNotaFiscal extends do Item

--- src/main/java/br/com/system/gestaoConstrucaoCivil/entity/almoxarifado/ItemNotaFiscal.java
+++ src/main/java/br/com/system/gestaoConstrucaoCivil/entity/almoxarifado/ItemNotaFiscal.java
@@ -2,42 +2,17 @@ package br.com.system.gestaoConstrucaoCivil.entity.almoxarifado;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-import org.springframework.data.jpa.domain.AbstractPersistable;
-
-import br.com.system.gestaoConstrucaoCivil.enuns.UnidadeMedidaEnum;
-
 @Entity
-@SequenceGenerator(name = "item_nota_fiscal_id_seq",
-sequenceName = "item_nota_fiscal_id_seq",
-initialValue = 1,
-allocationSize = 50)
 @Table(name = "item_nota_fiscal")
-public class ItemNotaFiscal implements Serializable {
+public class ItemNotaFiscal  extends Item implements Serializable  {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "item_nota_fiscal_id_seq")
-	private Long id;
 	
-	@ManyToOne
-	@JoinColumn(name = "id_produto", nullable = false)
-	private Produto produto;
-    @Column(nullable = false)
-	private Integer quantidade;
-	@Column(nullable = false)
-	private Double valorUnitario;
 	@Column(nullable = false)
 	private Double valorTotal;
 
@@ -46,39 +21,6 @@ public class ItemNotaFiscal implements Serializable {
 	private NotaFiscalProduto notaFiscalProduto;
 
 	
-	
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Produto getProduto() {
-		return produto;
-	}
-
-	public void setProduto(Produto produto) {
-		this.produto = produto;
-	}
-   public Integer getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(Integer quantidade) {
-		this.quantidade = quantidade;
-	}
-
-	public Double getValorUnitario() {
-		return valorUnitario;
-	}
-
-	public void setValorUnitario(Double valorUnitario) {
-		this.valorUnitario = valorUnitario;
-	}
-
 	public Double getValorTotal() {
 		return valorTotal;
 	}
@@ -95,29 +37,5 @@ public class ItemNotaFiscal implements Serializable {
 		this.notaFiscalProduto = notaFiscalProduto;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ItemNotaFiscal other = (ItemNotaFiscal) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
    
 }
