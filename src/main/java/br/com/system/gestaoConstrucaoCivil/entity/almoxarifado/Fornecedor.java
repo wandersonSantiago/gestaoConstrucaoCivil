9d233a38da7fe1721c3267725f58fa9155898e1e
Eduardo Ferrari <eduardo.gf@outlook.com.br>
2018-04-08 13:08:05 -0300

    Update Fornecedor.java

--- src/main/java/br/com/system/gestaoConstrucaoCivil/entity/almoxarifado/Fornecedor.java
+++ src/main/java/br/com/system/gestaoConstrucaoCivil/entity/almoxarifado/Fornecedor.java
@@ -1,10 +1,16 @@
 package br.com.system.gestaoConstrucaoCivil.entity.almoxarifado;
 
+import java.io.Serializable;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
@@ -15,14 +21,24 @@ import br.com.system.gestaoConstrucaoCivil.entity.DadoEmpresa;
 import br.com.system.gestaoConstrucaoCivil.findControll.FornecedorFindControll;
 
 @Entity
-@Table(name = "fornecedor")
-public class Fornecedor extends AbstractPersistable<Long> {
+@SequenceGenerator(name = "fornecedor_id_seq",
+sequenceName = "fornecedor_id_seq",
+initialValue = 1,
+allocationSize = 1)
+@Table(name = "fornecedor", schema = "almoxarifado")
+public class Fornecedor implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fornecedor_id_seq")
+	private Long id;
+	
 	@JsonView(FornecedorFindControll.class)
 	@Column(nullable = false)
 	private boolean ativo;
 	@JsonView(FornecedorFindControll.class)
-	@Column(nullable = true, length = 50)
+	@Column(nullable = false, length = 50)
 	private String contato;
 	@JsonView(FornecedorFindControll.class)
 	@Column(nullable = true, length = 50)
@@ -32,7 +48,17 @@ public class Fornecedor extends AbstractPersistable<Long> {
 	@OneToOne(cascade = {CascadeType.MERGE ,CascadeType.PERSIST})
 	@JoinColumn(name = "id_dado_empresa", nullable = false)
 	private DadoEmpresa dadoEmpresa;
-    
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getContato() {
 		return contato;
 	}
@@ -64,5 +90,12 @@ public class Fornecedor extends AbstractPersistable<Long> {
 	public void setAtivo(boolean ativo) {
 		this.ativo = ativo;
 	}
+
+	/*public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}*/
 
 }
