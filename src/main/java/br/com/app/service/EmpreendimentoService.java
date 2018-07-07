e660f554b34cbddddd70478f73c74f19b8a2f2e6
eduardoferrari01 <eduardo.gf@outlook.com.br>
2018-07-07 16:58:13 -0300

    Removido anotações

--- src/main/java/br/com/app/service/EmpreendimentoService.java
+++ src/main/java/br/com/app/service/EmpreendimentoService.java
@@ -1,9 +1,5 @@
 package br.com.app.service;
 
-
-
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,7 +13,6 @@ import br.com.app.exceptions.NotFoundException;
 import br.com.app.pojo.SessionUsuario;
 import br.com.app.repository.EmpreendimentoRepository;
 
-
 @Service
 @Transactional(readOnly = true, propagation = Propagation.REQUIRED)
 public class EmpreendimentoService {
@@ -25,75 +20,62 @@ public class EmpreendimentoService {
 	@Autowired
 	private EmpreendimentoRepository empreendimentoRepository;
 
-
-
 	@Transactional(readOnly = false)
-	public Empreendimento insert(Empreendimento empreendimento){
+	public Empreendimento insert(Empreendimento empreendimento) {
 		Usuario usuario = SessionUsuario.getInstance().getUsuario();
-		
-		if(!usuario.getLogin().equalsIgnoreCase("root")) {
-			
-			if(!usuario.getEmpreendimento().isMatriz()) {
-					throw new NotFoundException("Você não tem permissão para cadastrar um empreendimento!! ");
-						
+
+		if (!usuario.getLogin().equalsIgnoreCase("root")) {
+
+			if (!usuario.getEmpreendimento().isMatriz()) {
+				throw new NotFoundException("Você não tem permissão para cadastrar um empreendimento!! ");
+
 			}
-			empreendimento.setMatriz(usuario.getEmpreendimento());		
+			empreendimento.setMatriz(usuario.getEmpreendimento());
 		}
-		
+
 		return empreendimentoRepository.save(empreendimento);
 	}
-	
-	
-	public Empreendimento findById(Long id){
-		
+
+	public Empreendimento findById(Long id) {
+
 		return empreendimentoRepository.findById(id).orElseThrow(
 				() -> new NotFoundException("Não foi possivel encontrar nenhuma empreendimento com o ID: " + id));
 	}
 
-	
-	public Page<Empreendimento> findByDescricaoIgnoreCase(String descricao, Pageable page){
-		Page<Empreendimento> empreendimentos = null ;
-		
+	public Page<Empreendimento> findByDescricaoIgnoreCase(String descricao, Pageable page) {
+		Page<Empreendimento> empreendimentos = null;
+
 		Usuario user = SessionUsuario.getInstance().getUsuario();
-		if(user.isRoot()) {
-			empreendimentos =  empreendimentoRepository.findByDescricaoContainsIgnoreCaseAndMatrizIsNull(descricao, page);
-		}else
-			/*if(user.getEmpreendimento().isMatriz()){
-			empreendimentos =  empreendimentoRepository.findByMatriz_idAndDescricaoContainsIgnoreCase(user.getEmpreendimento().getId(),descricao,page);
-		}		*/
-		{
-			empreendimentos =  empreendimentoRepository.findByDescricaoContainsIgnoreCase(descricao,page);
+		if (user.isRoot()) {
+			empreendimentos = empreendimentoRepository.findByDescricaoContainsIgnoreCaseAndMatrizIsNull(descricao,
+					page);
+		} else {
+			empreendimentos = empreendimentoRepository.findByDescricaoContainsIgnoreCase(descricao, page);
 		}
-			
-		if(empreendimentos == null || empreendimentos.getNumberOfElements() < 1) {
-			throw new NotFoundException("Não foi possivel encontrar nenhuma empreendimento com esta descrição: " + descricao);
+
+		if (empreendimentos == null || empreendimentos.getNumberOfElements() < 1) {
+			throw new NotFoundException(
+					"Não foi possivel encontrar nenhuma empreendimento com esta descrição: " + descricao);
 		}
 		return empreendimentos;
-		
+
 	}
-	
-	
+
 	public Page<Empreendimento> findAll(Pageable page) {
-		
+
 		Page<Empreendimento> empreendimentos = null;
-				
+
 		Usuario user = SessionUsuario.getInstance().getUsuario();
-		if(user.isRoot()) {
-			empreendimentos =  empreendimentoRepository.findByMatrizIsNull(page);
-		}else {
-			empreendimentos =  empreendimentoRepository.findAll(page);
+		if (user.isRoot()) {
+			empreendimentos = empreendimentoRepository.findByMatrizIsNull(page);
+		} else {
+			empreendimentos = empreendimentoRepository.findAll(page);
 		}
-			/*if(user.getEmpreendimento().isMatriz()){
-			empreendimentos =  empreendimentoRepository.findByMatriz_id(user.getEmpreendimento().getId(),page);
-		}
-		*/
-		if( empreendimentos == null || empreendimentos.getNumberOfElements() < 1) {
+
+		if (empreendimentos == null || empreendimentos.getNumberOfElements() < 1) {
 			throw new NotFoundException("Não existe empreendimento cadastrada");
 		}
 		return empreendimentos;
 	}
-	
-
-	
 
 }
