c891fe0ce7047280bf1dff618bc18ff18f045c1f
eduardoferrari01 <eduardo.gf@outlook.com.br>
2018-07-08 06:02:49 -0300

    Removido import não utilizado

--- src/main/java/br/com/app/service/almoxarifado/RequisicaoService.java
+++ src/main/java/br/com/app/service/almoxarifado/RequisicaoService.java
@@ -1,7 +1,6 @@
 package br.com.app.service.almoxarifado;
 
 import java.util.Date;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
