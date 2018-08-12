6aca9165ae7c36337cda6c3c599a20ccc35e67db
eduardoferrari01 <eduardo.gf@outlook.com.br>
2018-08-12 14:21:27 -0300

    Removido import

--- src/main/java/br/com/app/web/controller/CategoriaRestController.java
+++ src/main/java/br/com/app/web/controller/CategoriaRestController.java
@@ -1,7 +1,6 @@
 package br.com.app.web.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
